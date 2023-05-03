d82729412d9a5b2cabcb5844be31c2cd61aef6fa
jessica gelsdorf <jegelsdorf@gmail.com>
2023-05-03 14:45:16 -0300

    arquivos do projeto, em criação

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+app.set('view engine', "ejs")
+
+const path = require('path')
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/detalhes', (req,res)=> {
+    const produtos = {
+        produto1: "Bicicleta 29 TSW E-Quest Full Carbon XT 12V Motor EP800 Shimano",
+        produto2: "BICICLETA 29 OGGI AGILE PRO CARBON 2023",
+        produto3: "BICICLETA 20 TSW RETRÔ ROSA",
+        produto4: "BICICLETA 29 ABSOLUTE NERO 4 ELITE 2X9V C/ HIDRÁULICO GRAFITE/PRETO"
+    }
+    const produto = req.query.produtos
+    res.render('detalhes', {produtos, produto})
+})
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+app.listen(3000, () => {
+    console.log("Servidor ligado na porta 3000!")
+})
