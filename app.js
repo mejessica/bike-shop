ba7a58331d035a5fb8d59e3f0684b13b0f739683
jessica gelsdorf <jegelsdorf@gmail.com>
2023-05-10 23:00:16 -0300

    arquivos do projeto com media querie

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/detalhes', (req,res)=> {
     const produtos = {
-        produto1: "Bicicleta 29 TSW E-Quest Full Carbon XT 12V Motor EP800 Shimano",
-        produto2: "BICICLETA 29 OGGI AGILE PRO CARBON 2023",
-        produto3: "BICICLETA 20 TSW RETRÔ ROSA",
-        produto4: "BICICLETA 29 ABSOLUTE NERO 4 ELITE 2X9V C/ HIDRÁULICO GRAFITE/PRETO"
+        produto1: "BICICLETA 29 OGGI AGILE PRO CARBON 2023",
+        produto2: "BICICLETA 20 TSW RETRÔ ROSA",
+        produto3: "BICICLETA 29 ABSOLUTE NERO 4 ELITE 2X9V C/ HIDRÁULICO GRAFITE/PRETO"
     }
     const produto = req.query.produtos
     res.render('detalhes', {produtos, produto})
---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+let imagem = document.getElementById('image1')
+.addEventListener("onMouseover",()=>{
+    document.imagem.setAttribute('src', '/images/bike2-2.jpg')
+})
+.addEventListener("onMouseout",()=>{
+    document.imagem.setAttribute('src', '/images/bike2.jpg')
+})
